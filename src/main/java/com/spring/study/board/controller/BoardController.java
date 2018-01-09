6a1b9d44c3d9ac1ce95fbaa094702da4ca4b5a21
duckheewon <fain9301@yahoo.com>
2018-01-09 16:12:03 +0900

    fix board

--- src/main/java/com/spring/study/board/controller/BoardController.java
+++ src/main/java/com/spring/study/board/controller/BoardController.java
@@ -1,4 +1,4 @@
-package com.study.board.controller;
+package com.spring.study.board.controller;
 
 
 import java.util.List;
@@ -14,10 +14,14 @@ import com.spring.study.board.model.vo.ReplyVO;
 import com.spring.study.board.model.vo.BoardVO;
 import com.spring.study.board.service.BoardService;
 
+
+
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+	
+	
 	@Resource(name="BoardService")
 	private BoardService service;
 	
@@ -26,6 +30,7 @@ public class BoardController {
 	{
 		System.out.println("list page controllers");
 		List<BoardVO> list = service.list();
+		System.out.println("list : " + list.toString());
 		model.addAttribute("list", list);
 		return "board/listPage";
 	}
