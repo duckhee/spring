f802de0f1bfedfeaa2c9f334f3c30020993e1409
duckheewon <fain9301@yahoo.com>
2018-01-09 16:38:23 +0900

    board set

--- src/main/java/com/spring/study/board/dao/BoardDaoImpl.java
+++ src/main/java/com/spring/study/board/dao/BoardDaoImpl.java
@@ -1,6 +1,8 @@
 package com.spring.study.board.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,49 +29,79 @@ public class BoardDaoImpl implements BoardDao {
 	public int insertRow(BoardVO obj) {
 		// TODO Auto-generated method stub
 		System.out.println("Dao insertRow");
-		return 0;
+		return session.insert("insert", obj);
+	}
+	
+	private void upCnt(BoardVO obj) {
+		System.out.println("Dao upCnt");
+		session.update("com.spring.acorn.mapper.board.upcnt", obj);
+		/*
+		 * UPDATE TBL_BOARD 
+		 * SET VIEWCNT = VIEWCNT + 1
+		 * WHERE BNO = #{bno}
+		 */
 	}
 
 	@Override
 	public BoardVO readRow(BoardVO obj) {
 		// TODO Auto-generated method stub
 		System.out.println("Dao readRow");
-		return null;
+		upCnt(obj);
+		System.out.println("Dao param : "+obj.getBno());
+		BoardVO board = session.selectOne("com.spring.acorn.mapper.board.read", obj);
+		List<ReplyVO> rlist = session.selectList("read", obj);
+		board.setRlist(rlist);
+		return board ; 
 	}
 
 	@Override
 	public int removeRow(BoardVO obj) {
 		// TODO Auto-generated method stub
 		System.out.println("Dao removeRow");
-		return 0;
+		System.out.println("Dao param : "+obj.getBno());
+		return session.delete("remove", obj);
+		
 	}
 
 	@Override
 	public int modifyRow(BoardVO obj) {
 		// TODO Auto-generated method stub
 		System.out.println("Dao modifyRow");
-		return 0;
+		return session.update("modify", obj);
 	}
 
 	@Override
 	public List<BoardVO> searchRow(String type, String keyword) {
 		// TODO Auto-generated method stub
 		System.out.println("Dao searchRow");
-		return null;
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("type", type);	map.put("keyword", keyword);
+		return session.selectList("search", map);
 	}
 
 	@Override
 	public List<ReplyVO> rInsertRow(ReplyVO reply) {
 		// TODO Auto-generated method stub
 		System.out.println("Dao rInsertRow");
-		return null;
+		int flag = session.insert("rInsert" , reply); 
+		List<ReplyVO> rlist = null ; 
+		if(flag!=0) {
+			rlist = session.selectList("readReply", reply);
+		}
+		return rlist ;
 	}
 
 	@Override
 	public List<ReplyVO> rRemoveRow(ReplyVO reply) {
 		// TODO Auto-generated method stub
 		System.out.println("Dao rRemoveRow");
-		return null;
+
+		int flag = session.delete("rRemove" , reply); 
+		List<ReplyVO> rlist = null ; 
+		if(flag!=0) {
+			rlist = session.selectList("readReply", reply);
+		}
+		return rlist ;
 	}
 
 }
